f50d9db39cc65e587f97541d98240645a546460f
samuelm235 <108957962+samuelm235@users.noreply.github.com>
2024-10-23 13:31:27 +0000

    2D array set, started getNextlLoc

--- GridPath.java
+++ GridPath.java
@@ -1,7 +1,23 @@
 public class GridPath {
     /** Initialized in the constructor with distinct values that never change */
     private int[][] grid;
-
+    public GridPath(int[][] values)
+    {
+        grid = values;
+    }
+    public String toString()
+    {
+        String s = "";
+        for (int row = 0; row < grid.length; row++)
+        {
+            for (int col = 0; col < grid[0].length; col++)
+            {
+                s += grid[row][col] + " ";
+            }
+            s += "\n";
+        }
+        return s;
+    }
     /**
      * Returns the Location representing a neighbor of the grid element at row and
      * col,
@@ -11,8 +27,13 @@ public class GridPath {
      * row and col do not specify the element in the last row and last column of
      * grid.
      */
-    public Location getNextLoc(int row, int col) {
-        /* to be implemented in part (a) */ }
+    public Location getNextLoc(int row, int col)
+     {
+        int below = grid[row + 1][col];
+        int right = grid[row][col + 1];
+        return null;
+        /* to be implemented in part (a) */ 
+    }
 
     /**
      * Computes and returns the sum of all values on a path through grid, as
@@ -24,5 +45,6 @@ public class GridPath {
      * grid.
      */
     public int sumPath(int row, int col) {
+        return 0;
         /* to be implemented in part (b) */ }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 public class Main{
     public static void main(String[] args) {
-        
+        int[][] nums = 
+        {
+            {12, 3, 4, 13, 5},
+            {11, 21, 2, 14, 16},
+            {7, 8, 9, 15, 0},
+            {10, 17, 20, 19, 1},
+            {18, 22, 30, 25, 6}
+        };
+        GridPath one = new GridPath(nums);
+        System.out.println(one);
     }
 }
